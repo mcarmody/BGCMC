53e9642a0da2e96b9b7b30a6acdbd64c433a30d2
mcarmody <Mike@Michaels-MacBook-Pro.local>
2015-02-09 15:27:08 -0600

    further styling and coloring on dropdowns

--- js/app.js
+++ js/app.js
@@ -3,10 +3,23 @@
 $(document).ready( function () {
 
 	$(".nav-item").on("click", function() {
-		if($(this).find(".dropNav").is(":hidden")) {
-		    $(this).find(".dropNav").slideDown(500);
+		if($(this).find(".hiddenNav").is(":hidden")) {
+		    $(this).find(".hiddenNav").slideDown(500)
+		    $(this).css("background-color", "#008CBA").find("a").css("color", "white");
+		    delay(500);
+		    $(this).find(".hiddenNav").addClass("droppedNav").removeClass("hiddenNav");
 		} else {
-			$(this).find(".dropNav").slideUp(300);
+			$(this).find(".hiddenNav").slideUp(300);
+		 	$(this).css("background-color", "white")
+			$(this).find("a").removeAttr("style");
+		};
+		if($(this).find(".droppedNav").is(":hidden")) {
+		    $(this).find(".droppedNav").slideDown(500)
+		    .addClass("hiddenNav").removeClass("droppedNav");
+		 	$(this).css("background-color", "#008CBA").find("a").css("color", "white");
+		} else {
+			$(this).find(".droppedNav").slideUp(300)
+			$(this).find("a").removeAttr("style");
 		};
 	});
 
@@ -17,12 +30,12 @@ $(document).ready( function () {
 	    if (scroll>10) {
 	    	$("#navbar").css("border-bottom", "1px solid #999");
 	    } if (scroll>20) {
-	    	$(".dropNav").css("border", "1px solid #999");
-    		$(".dropNav").css("border-top", "none");
+	    	$(".hiddenNav, .droppedNav").css("border", "1px solid #999")
+	    	.css("border-top", "none");
 		} else {
     		$("#navbar").css("border-bottom", "1px solid white");
-	    	$(".dropNav").css("border", "1px solid white");
-	    	$(".dropNav").css("border-top", "none");
+    		$(".hiddenNav, .droppedNav").css("border", "1px solid #008CBA")
+	    	.css("border-top", "none");
 		};
 
 		if ($(window).width()<481) {
