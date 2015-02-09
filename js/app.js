19fc6b145a3386e6e7152cd8f7a6de3846d56ad3
mcarmody <Mike@Michaels-MacBook-Pro.local>
2015-02-09 02:05:31 -0600

    removing foundation to rebuild from scratch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).ready();
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
